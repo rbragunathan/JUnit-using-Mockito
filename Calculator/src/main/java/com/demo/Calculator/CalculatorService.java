05c67b7996acac51d53a4b7e654185e894bbf70c
rbragunathan <47302846+rbragunathan@users.noreply.github.com>
2019-02-22 11:15:10 +0530

    Add files via upload

---
+++ Calculator/src/main/java/com/demo/Calculator/CalculatorService.java
@@ -0,0 +1,6 @@
+package com.demo.Calculator;
+
+public interface CalculatorService {
+
+	public int add(int i,int j);
+}
---
+++ Calculator/src/test/java/com/demo/Calculator/TestCalculator.java
@@ -0,0 +1,38 @@
+package com.demo.Calculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+
+public class TestCalculator {
+
+	Calculator c = null;
+	@Mock
+	CalculatorService service;
+	
+	@Rule 
+	public MockitoRule rule = MockitoJUnit.rule();
+	
+	@Before
+	public void setUp()
+	{
+		c=new Calculator(service);
+	}
+	
+	@Test
+	public void testPerform()
+	{
+		when(service.add(2, 3)).thenReturn(5);
+		assertEquals(10,c.perform(2, 3));
+		verify(service).add(2,3);
+	}
+}
